2d87b610afa6dd84e669ed083285eda70b4e37a9
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-22 17:52:24 +0100

    inizio stilizzazione

--- my.js
+++ my.js
@@ -1,15 +1,15 @@
 const distance = document.getElementById('distance');
-const age = document.getElementById('anni');
+const age = document.getElementById('age');
 const button = document.querySelector('button');
 
-let price = 0.27;
+let price = 0.2762;
 
 button.addEventListener('click', function(){
     let costo= price * distance.value;
-    if (age < 18){
-        costo= costo*0.8;
-    }else if (age > 65){
-        costo= costo*0.6;
+    if (age.value < 18){
+        costo= costo*(100-17.5)/100;
+    }else if (age.value > 65){
+        costo= costo*(100-33.3)/100;
     }
     document.getElementById('output').innerHTML= costo.toFixed(2) + '&euro;';
 })
