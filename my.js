3107391d80cd442fd957515b5d50b64af06f4a14
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-22 15:57:04 +0100

    ulteriori modifiche

--- my.js
+++ my.js
@@ -1,7 +1,15 @@
-const distance = document.querySelector('input#distance');
-const age = document.querySelector('input#anni');
+const distance = document.getElementById('input#distance');
+const age = document.getElementById('input#anni');
 const button = document.querySelector('button');
 
+let price = 0.27;
+let costo= price * distance;
+
 button.addEventListener('click', function(){
-    alert('cliccato');
+    if (age < 18){
+        costo= costo*0.82;
+    }else if (age > 65){
+        costo= costo*0.66;
+    }
+    document.getElementById('output').innerHTML=costo.toFixed(2) + '&euro;';
 })
