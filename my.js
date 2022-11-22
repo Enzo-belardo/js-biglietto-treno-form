4847413c83495dcde3b2b298e80109af62451b48
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-22 15:36:22 +0100

    primo push

---
+++ my.js
@@ -0,0 +1,7 @@
+const distance = document.querySelector('input#distance');
+const age = document.querySelector('input#anni');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function(){
+    alert('cliccato');
+})
